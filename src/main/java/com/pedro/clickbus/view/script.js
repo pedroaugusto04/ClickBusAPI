70d3e3f87f25c847927c7998434de7798989d0f1
pedroaugusto04 <pedroaugustoaduarte@gmail.com>
2023-01-29 09:36:15 -0300

    front-end connected with back-end

--- src/main/java/com/pedro/clickbus/view/script.js
+++ src/main/java/com/pedro/clickbus/view/script.js
@@ -1,10 +1,13 @@
 const buttonSubmit = document.querySelector("#buttonSubmit");
 const buttonGet = document.querySelector("#buttonGet");
 const buttonUpdate = document.querySelector("#buttonUpdate");
+const buttonGetByName = document.querySelector("#buttonGetName");
 const Iname = document.querySelector(".name");
 const Islug= document.querySelector(".slug");
 const Icity = document.querySelector(".city");
 const Istate = document.querySelector(".state");
+const responseDiv = document.querySelector(".response");
+
 
 buttonSubmit.addEventListener("click",function(event){
     event.preventDefault();
@@ -14,7 +17,7 @@ buttonSubmit.addEventListener("click",function(event){
 
 buttonGet.addEventListener("click",function(event){
     event.preventDefault();
-    getPlace();
+    getPlaces();
     clean();
 });
 
@@ -25,8 +28,17 @@ buttonUpdate.addEventListener("click",function(event){
     clean();
 });
 
+buttonGetByName.addEventListener("click",function(event){
+    event.preventDefault();
+    getPlaceByName();
+    clean();
+})
+
+
+
  function createPlace(){
-    fetch("http://localhost:8080/api/places",
+    if (validateAll()){
+        fetch("http://localhost:8080/api/places",
     {
         headers: {
             "Accept": "application/json",
@@ -39,39 +51,75 @@ buttonUpdate.addEventListener("click",function(event){
             city: Icity.value,
             state: Istate.value
         })
-    }).then(function(res) {console.log(res)})
-    .catch(function(error){console.log(error)})
+    })
+    .then(response => response.json())
+    .then(data => {
+        dataText = JSON.stringify(data);
+        responseDiv.textContent = dataText;
+    })
+    .catch(error => console.log(error))
+    } else {
+        alert("All fields are required")
+    }
 };
 
-function getPlace(){
-    fetch("http://localhost:8080/api/places",
-    {
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-        },
-        method: "GET"
-    }).then(function(res) {console.log(res)})
-    .catch(function(error){console.log(error)})
+function getPlaceByName(){
+    if (validateName()){
+        fetch("http://localhost:8080/api/places/name/" + Iname.value)
+        .then(response => response.json())
+        .then(data => {
+            dataText = JSON.stringify(data);
+            responseDiv.textContent = dataText;
+        })
+        .catch(error => console.log(error))  
+    } else {
+        alert("Name is required");
+    }
+};
+
+ function getPlaces(){
+    fetch("http://localhost:8080/api/places")
+    .then(response => response.json())
+    .then(data => {
+        dataText = JSON.stringify(data);
+        responseDiv.textContent = dataText;
+    })
+    .catch(error => console.log(error))
 };
 
 
 function updatePlace(){
-    let place = {};
-    place.name = Iname.value;
-    place.slug = Islug.value;
-    place.city = Icity.value;
-    place.state = Istate.value;
-    fetch("http://localhost:8080/api/places/123",
-    {
+    if (validateAll()){
+        let place = {};
+        place.name = Iname.value;
+        place.city = Icity.value;
+        place.state = Istate.value;
+        fetch("http://localhost:8080/api/places/" + Islug.value,
+        {
         headers: {
             "Accept": "application/json",
             "Content-Type": "application/json"
         },
         method: "PUT",
         body: JSON.stringify({place})
-    }).then(function(res) {console.log(res)})
-    .catch(function(error){console.log(error)})
+        })
+        .then(response => {
+        if (response.status === 204){
+            responseDiv.textContent = `${response.status}: No Content`;
+        } else {
+            return response.json();
+        }
+        })
+        .then(data => {
+        if(data){
+            dataText = JSON.stringify(data);
+            responseDiv.textContent = dataText;  
+        }
+        })
+        .catch(error => console.log(error))  
+    } else {
+        alert("All fields are required");
+    }
 };
 
 function clean(){
@@ -79,4 +127,26 @@ function clean(){
     Islug.value = "";
     Icity.value = "";
     Istate.value = "";
+}
+
+
+function validateName(){
+    if (Iname.value.trim() == ""){
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validateAll(){
+    if (Iname.value.trim() == "" || Islug.value.trim() == "" || Icity.value.trim() == "" || Istate.value.trim() == ""){
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
+function format(dataText){
+    //
 }
